77da635d0f12cf6540ef5457b41b51ec88e3d74d
Bhagya1315 <bhagyalakshmiutukuri@gmail.com>
2026-03-16 18:53:31 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,37 @@
+'''age=18
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible")'''
+
+
+#even or odd
+'''def check_num(n):
+    if n%2==0:
+        print('Even')
+    else:
+        print('Odd')
+def main():
+    n=int(input("Enter number: "))
+    check_num(n)
+main()'''
+
+
+
+
+#Maximum number
+'''def find_max(a,b):
+    if a>b:
+        print("Maximum: ",a)
+    else:
+        print("Maximum: ",b)
+def main():
+    a=int(input("Enter a number: "))
+    b=int(input("Enter b number: "))
+    find_max(a,b)
+main()'''
+
+
+
+#Student grading system
+
