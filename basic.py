4414c7a6e48997f8588f9ba7d133268ede5625f1
Bhagya1315 <bhagyalakshmiutukuri@gmail.com>
2026-03-16 18:56:14 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,37 @@
+'''for i in range(1,6):
+    print(i)'''
+
+    
+'''i=1
+while i<=5:
+    print(i)
+    i+=1'''
+
+'''n=int(input('Enter the number: '))
+for i in range(1,n+1):
+    print(i)'''
+
+
+'''n=int(input('Enter the number: '))
+i=1
+while i<=n:
+    print(i)
+    i+=1'''
+
+
+#sum of N numbers
+'''def calculate_sum(n):
+    total=0
+    for i in range(1,n+1):
+        total+=i
+    return total
+def main():
+    n=int(input("Enter number: "))
+    print("sum: ",calculate_sum(n))
+main()'''
+
+
+
+
+    
+  
