f9e82d1d5c767cdcc162ad23d0ba37285194a047
Bhagya1315 <bhagyalakshmiutukuri@gmail.com>
2026-04-22 21:58:59 +0530

    Add files via upload

---
+++ double linked list.py
@@ -0,0 +1,203 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.data = data
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def inset_at_beginning(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    #Inserting operations
+    #inserting at Begininng
+    def insert_begin(self,data):
+        new_node = Node(data)
+
+        if self.head:
+            self.head.prev = new_node
+            new_node.prev = self.head
+
+        self.head = new_node
+
+    def insert_end(self,data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+        new_node.prev = temp
+
+    def insert_at_pos(self, pos, data):
+            new_node = Node(data)
+
+            if pos == 0:
+                self.insert_begin(data)
+                return
+
+            temp = self.head
+            for _ in range(pos - 1):
+                if temp is None:
+                    return
+            temp = temp.next
+
+
+
+            if temp.next:
+                temp.next.prev = new_node
+                new_node.prev = temp.next
+
+            temp.next = new_node
+            new_node.prev = temp
+
+
+     #Deletion
+
+    def delete_begin(self):
+        if self.head is None:
+            return
+
+        self.head = self.head.next
+
+        if self.head:
+            self.head.prev = None
+
+    def delete_end(self):
+        if self.head is None:
+             return
+
+        if self.head.next is None:
+           self.head = None
+           return
+        temp = self.head
+        while temp.next:
+             temp = temp.next
+        temp.prev.next = None
+    def delete_value(self, key):
+        if self.head is None:
+             return
+
+        temp = self.head
+
+        while temp:
+            if temp.data == key:
+
+                if temp.prev:
+                     temp.prev.next = temp.next
+
+                else:
+                     self.head = temp.next
+
+                if temp.next:
+                     temp.next.prev = temp.prev
+    
+                return
+            temp = temp.next
+    def delete_at_position(self, pos):
+         if self.head is None:
+             return
+         temp = self.head
+
+         if pos ==0:
+            self.delete_begin()
+            return
+
+         for _ in range(pos):
+             if temp is None:
+                 return
+             temp = temp.next
+
+         if temp.prev:
+             temp.prev.next = temp.next
+         if temp.next:
+             temp.next.prev = temp.prev
+
+    #Access Opearations
+    #Get By Position
+    def  get(self, pos):
+         temp = self.head
+         for _ in range(pos):
+             if temp is None:
+                 return None
+             temp = temp.next
+         return temp.data if temp else None
+
+    
+    #Search
+    def search(self, key):
+        temp = self.head
+        while temp:
+           if temp.data == key:
+                return True
+           temp = temp.next
+        return False
+
+    #Update by Position
+    def update(self,pos, value):
+        temp = self.head
+
+        for _ in range(pos):
+            if temp is None:
+              return
+        temp = temp.next
+
+        if temp:
+            temp.data = value
+    def update_value(self, old, new):
+        temp = self.head
+
+        while temp:
+            if temp.data == old:
+                temp.data = new
+                return
+            temp = temp.next
+    def display(self):
+        temp = self.head
+        while temp:
+             print(temp.data,end='<->')
+             temp = temp.next
+        print("None")
+
+
+dll=DoublyLinkedList()
+
+dll.insert_begin(10)
+dll.insert_begin(5)
+dll.insert_end(20)
+dll.insert_end(30)
+
+dll.display()
+
+dll.insert_at_pos(2,15)
+
+dll.display()
+
+print(dll.get(2))
+print(dll.search(20))
+
+dll.delete_value(15)
+dll.display()
+
+dll.update(1,99)
+dll.display()
+    
+
+
+
+
+        
+
+
+
+
+
+
+                
